7b95515ed2cabe3502ae4eacd0c275a7313f65a7
lyt_git <2119444172@qq.com>
2022-04-16 02:41:01 +0800

    first_commit_java

---
+++ 1_HELLO_WORLD/src/hello/HelloWorld.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
