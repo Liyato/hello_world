380a9c3995b960edc1b5de708d21758273bf5f42
lyt_git <2119444172@qq.com>
2022-04-16 02:44:33 +0800

    second_commit_java

--- 1_HELLO_WORLD/src/hello/HelloWorld.java
+++ 1_HELLO_WORLD/src/hello/HelloWorld.java
@@ -14,5 +14,6 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello worldtest");
     }
 }
